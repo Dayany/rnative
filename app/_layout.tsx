9c64195a9e9b6c5bb4dc6b44dc763bda9b37c261
Dayan <yamindayan@gmail.com>
2024-08-22 02:33:48 -0400

    Setup initial views and nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Stack } from "expo-router";
 
-export default function App() {
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default RootLayout;
